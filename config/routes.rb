fb01d01ee1ea2502bf2c86c88196802dd51d5f88
wentingzoe <ywtzoe@gmail.com>
2022-02-06 12:34:29 -0500

    Create get/signup and post/users routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
+   # Sign-up
+   get '/signup' => 'users#new'
+   post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
