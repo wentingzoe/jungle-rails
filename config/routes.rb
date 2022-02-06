6c70599cb9c474b1b7a5702fe861699ca4219de8
wentingzoe <ywtzoe@gmail.com>
2022-02-06 13:06:26 -0500

    Add login logout in routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,10 @@ Rails.application.routes.draw do
    get '/signup' => 'users#new'
    post '/users' => 'users#create'
 
+   # Login/Logout
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
