006b382663918fdc248572389f704cb2dc416f4c
wentingzoe <ywtzoe@gmail.com>
2022-02-06 13:48:42 -0500

    Add redirect to login page when user fair the log in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,6 +10,9 @@ class SessionController < ApplicationController
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
+    else
+      # If user's login doesn't work, send them back to the login form.
+        redirect_to '/login'
    
     end
   end
