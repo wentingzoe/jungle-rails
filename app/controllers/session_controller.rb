69a3bcd1c0d9302190903a782740d7cee7773769
wentingzoe <ywtzoe@gmail.com>
2022-02-06 13:41:47 -0500

    Add logic to session create and destroy

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,20 @@
 class SessionController < ApplicationController
   def new
   end
-  def create
+
+ def create
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+   
+    end
   end
 
   def destroy
+    session[:user_id] = nil
   end
 end
