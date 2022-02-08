7b6070e54895560a6c45f4224b4f2ae75711b66a
wentingzoe <ywtzoe@gmail.com>
2022-02-07 20:20:52 -0500

    Add user_spec, update user and sessions_controller with password and email condicaton

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,15 @@ class SessionsController < ApplicationController
   def new
   end
 
- def create
-    user = User.find_by_email(params[:email])
-
-    if user && user.authenticate(params[:password])
+def create
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
-        redirect_to '/login'
-   
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
     end
   end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+
+    it 'is valid with matching password and password_confirmation fields' do
+      @user = User.new(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+      expect(@user).to be_valid
+    end
+
+    it 'is invalid with non-matching password and password_confirmation fields' do
+      @user = User.new(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalals")
+      expect(@user).to_not be_valid
+    end
+
+    it 'is valid with unique emails' do 
+       @user1 = User.new(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+      expect(@user1).to be_valid
+       @user2 = User.new(name: "Yeah", email: "happy@456.com", password:"lalala", password_confirmation:"lalala")
+      expect(@user2).to be_valid
+    end
+
+    it 'is invalid with repeated emails' do 
+      @user1 = User.new(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+      expect(@user1).to be_valid
+      @user1.save!
+      @user2 = User.new(name: "Yeahh", email: "happy@123.com", password:"yoyoyo", password_confirmation:"yoyoyo")
+      expect(@user2).to_not be_valid
+    end
+
+    it 'must have a minimum password length when a user account is being created' do
+      @user = User.new(name: "Yeah", email: "happy@123.com", password:"yoyo", password_confirmation:"lalala")
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires emails and name' do
+      @user = User.new(name:nil, email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+      expect(@user).to_not be_valid
+    end
+  end
+
+   describe '.authenticate_with_credentials' do
+      it 'must login when a registered user provides the correct email and password' do
+          @user = User.create(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+        expect(User.authenticate_with_credentials("happy@123.com", "lalala")).to eql(@user)
+      end
+
+      it 'must login and be case insensitive for e-mails' do
+        @user = User.create(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+        expect(User.authenticate_with_credentials("HAPPY@123.com", "lalala")).to eq(@user)
+      end
+
+      it 'must login and ignore leading and trailing spaces for e-mails' do
+          @user = User.create(name: "Yeah", email: "happy@123.com", password:"lalala", password_confirmation:"lalala")
+        expect(User.authenticate_with_credentials("     happy@123.com", "lalala")).to eq(@user)
+      end
+   end
+
+ 
 end
