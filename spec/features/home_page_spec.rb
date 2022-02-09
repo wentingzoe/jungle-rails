10a55baa410fd0c7ad8d6620841467220701b7ab
wentingzoe <ywtzoe@gmail.com>
2022-02-08 20:07:32 -0500

    Generate feature sepec for products home page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "HomePages", type: :feature do
+ require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+  end
+
+end
+end
